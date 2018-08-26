bb30c2ed2e2d0e21af554f55663a964af940420c
dragonrider1024 <xuzhiwen1024@gmail.com>
2018-08-26 17:07:07 -0400

    readme updated and comment added

--- src/prediction-validation.py
+++ src/prediction-validation.py
@@ -17,57 +17,63 @@ class Solution:
     windowFileInput.close()
     comparisonFileOutput = open(comparisonFile, 'w')
     x = actualFileInput.tell() # remember the first line position
-    startTimeStamp = int(actualFileInput.readline().split('|')[0])
-    actualFileInput.seek(x)
+    startTimeStamp = int(actualFileInput.readline().split('|')[0])  #get start time
+    actualFileInput.seek(x) #go back to first line
     timeStamp = startTimeStamp
-    totalNotIgnore = 0
-    totalError = 0
-    formatString = "%d|%d|%.2f\n"
-    errorQueue = deque()
-    while timeStamp < startTimeStamp + window:
+    totalNotIgnore = 0 #total number of non-ignore entries in current time window
+    totalError = 0 #total error in current time window
+    avgError = 0 #average error in current time window
+    formatString = "%d|%d|%.2f\n" #format output string
+    errorQueue = deque() # error queue that stores the error and number of entries that are not 'ignore' at particular time stamp
+    endTimeStamp = startTimeStamp + window - 1
+    while timeStamp <= endTimeStamp:
       timeStamp, notIgnore, error = self.readLinesForTime(actualFileInput, predictFileInput, timeStamp)
-      errorQueue.append((notIgnore, error))
-      totalNotIgnore += notIgnore
-      totalError += error
-    comparisonFileOutput.write(formatString % (startTimeStamp, startTimeStamp + window - 1, totalError / totalNotIgnore))
-    startTimeStamp += 1
+      errorQueue.append((notIgnore, error)) #push the current error and number of non-ignore entries into the queue
+      totalNotIgnore += notIgnore #add number of non-ignore entries to the total count
+      totalError += error #add current error to the total error
+    avgError = (totalError / totalNotIgnore) # calculate the average error at current time window
+    comparisonFileOutput.write(formatString % (startTimeStamp, endTimeStamp, avgError)) #write to output
+    startTimeStamp += 1 #slide time window
+    endTimeStamp += 1 #slide time window
     while timeStamp != 'end-of-file':
       timeStamp, notIgnore, error = self.readLinesForTime(actualFileInput, predictFileInput, timeStamp)
-      errorQueue.append((notIgnore, error))
-      prevNotIgnore, prevError = errorQueue.popleft()
-      totalNotIgnore = totalNotIgnore - prevNotIgnore + notIgnore
-      totalError = totalError - prevError + error
-      comparisonFileOutput.write(formatString % (startTimeStamp, startTimeStamp + window - 1, totalError / totalNotIgnore))
-      startTimeStamp += 1
-      
-
-    
+      errorQueue.append((notIgnore, error)) #push the current error and number of non-ignore entries into the queue
+      prevNotIgnore, prevError = errorQueue.popleft() #pop the item whose time is not in current window anymore
+      totalError = totalError - prevError + error # deduct the error that not in current window anymore and add new error
+      totalNotIgnore = totalNotIgnore - prevNotIgnore + notIgnore # deduct the number of non-ignore entries not in current window anymore, add new
+      avgError = totalError / totalNotIgnore # calculate the average error for current time window
+      comparisonFileOutput.write(formatString % (startTimeStamp, endTimeStamp, avgError)) #write to output
+      startTimeStamp += 1 #slide the time window
+      endTimeStamp += 1 #slide the time window
     
     
 
   def readLinesForTime(self, actualFileInput, predictFileInput, timeStamp):
+    ''' read lines corresponding to timeStamp and calculate the errors and number of non-ignore entries'''
+#    print timeStamp
     actualDict = {}
     predictDict = {}
     errorDict = {}
 
     rowString = predictFileInput.readline()
-    while rowString and rowString != "" and rowString != '\n' and int(rowString.split('|')[0]) == timeStamp:
+
+    while rowString != None and rowString != "" and rowString != '\n' and int(rowString.split('|')[0]) == timeStamp:
       stockName = rowString.split('|')[1]
       stockPrice = float(rowString.split('|')[2])
       predictDict[stockName] = stockPrice
       x = predictFileInput.tell()
       rowString = predictFileInput.readline()
-    if rowString and rowString != "" and rowString != '\n':
+    if rowString != None and rowString != "" and rowString != '\n':
       predictFileInput.seek(x)
 
     rowString = actualFileInput.readline()
-    while rowString and rowString != "" and rowString != '\n' and int(rowString.split('|')[0]) == timeStamp:
+    while rowString != None and rowString != "" and rowString != '\n' and int(rowString.split('|')[0]) == timeStamp:
       stockName = rowString.split('|')[1]
       stockPrice = float(rowString.split('|')[2])
       actualDict[stockName] = stockPrice
       x = actualFileInput.tell()
       rowString = actualFileInput.readline()
-    if rowString and rowString != "" and rowString != '\n':
+    if rowString != None and rowString != "" and rowString != '\n':
       timeStamp = int(rowString.split('|')[0])
       actualFileInput.seek(x)
     else:
@@ -84,6 +90,13 @@ class Solution:
       if errorDict[key] != 'ignore':
         notIgnore += 1
         error += errorDict[key]
+#    print len(errorDict), notIgnore, error
+#    for key in actualDict:
+#      if key not in predictDict:
+#        print key, actualDict[key], 'NA', errorDict[key]
+#      else:
+#        print key, actualDict[key], predictDict[key], errorDict[key]
+ 
     return timeStamp, notIgnore, error
 
 if __name__ == "__main__":
